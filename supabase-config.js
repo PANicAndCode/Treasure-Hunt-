bfdeca6e93ff1f7fd426efd3435d69942fe12b76
PANicAndCode <totallynotascamboi@gmail.com>
2026-04-09 18:54:03 -0500

    Add generic treasure hunt site files

---
+++ supabase-config.js
@@ -0,0 +1,5 @@
+
+window.SUPABASE_CONFIG = {
+  url: "https://eyzrvijtlfhnsnsvxtaj.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5enJ2aWp0bGZobnNuc3Z4dGFqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ1NzY0MTIsImV4cCI6MjA5MDE1MjQxMn0.WDLt-0nTyDY0ILr54zKvagaIDJo-WRVKwHL4RLrDy-s"
+};
